9899a2e61c03fcf0d6016d588c2990d91c938588
Illya <romanenkoisnot@gmail.com>
2024-08-05 17:59:46 +0300

    delete: product form widget
    was implemented by other widget

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductsListComponent } from "../../widgets/products-page/products-list/products-list.component";
 import { ProductOverviewComponent } from "../../widgets/products-page/product-overview/product-overview.component";
-import { ProductFormComponent } from "../../widgets/products-page/product-form/product-form.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [ProductsListComponent, ProductOverviewComponent, ProductFormComponent],
+  imports: [ProductsListComponent, ProductOverviewComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
